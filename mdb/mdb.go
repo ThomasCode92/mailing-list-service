7d1ceadc1c980cfaeb7ed5fb6d5b80bf0dfc8f6a
ThomasCode92 <thomascode92@hotmail.com>
2023-01-04 12:33:31 +0100

    Implementing CRUD Operations

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -15,6 +15,11 @@ type EmailEntry struct {
 	OptOut      bool
 }
 
+type GetEmailBatchQueryParams struct {
+	Page  int
+	Count int
+}
+
 func TryCreate(db *sql.DB) {
 	_, err := db.Exec(`
 		CREATE TABLE emails (
@@ -53,4 +58,105 @@ func emailEntryFromRow(row *sql.Rows) (*EmailEntry, error) {
 	time := time.Unix(confirmedAt, 0)
 
 	return &EmailEntry{Id: id, Email: email, ConfirmedAt: &time, OptOut: optOut}, nil
+}
+
+func CreateEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+		INSERT INTO
+		emails(email, confirmed_at, opt_out)
+		VALUES(?, 0, false);
+	`, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func GetEmail(db *sql.DB, email string) (*EmailEntry, error) {
+	rows, err := db.Query(`
+		SELECT id, email, confirmed_at, opt_out
+		FROM emails
+		WHERE email = ?
+	`, email)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		return emailEntryFromRow(rows)
+	}
+
+	return nil, nil
+}
+
+func UpdateEmail(db *sql.DB, entry EmailEntry) error {
+	time := entry.ConfirmedAt.Unix()
+
+	_, err := db.Exec(`
+		INSERT INTO
+		emails(email, confirmed_at, opt_out)
+		VALUES(?, ?, ?)
+		ON CONFLICT(email) DO UPDATE SET
+		  confirmed_at=?,
+		  opt_out=?
+	`, entry.Email, time, entry.OptOut, time, entry.OptOut)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func DeleteEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+		UPDATE emails
+		SET opt_out=true
+		WHERE email=?
+	`, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func GetEmailBatch(db *sql.DB, params GetEmailBatchQueryParams) ([]EmailEntry, error) {
+	var empty []EmailEntry
+
+	rows, err := db.Query(`
+		SELECT id, email, confirmed_at, opt_out
+		FROM emails
+		WHERE opt_out = false
+		ORDER BY id ASC
+		LIMIT ? OFFSET ?
+	`, params.Count, (params.Page-1)*params.Count)
+
+	if err != nil {
+		log.Println(err)
+		return empty, err
+	}
+
+	defer rows.Close()
+
+	emails := make([]EmailEntry, 0, params.Count)
+
+	for rows.Next() {
+		email, err := emailEntryFromRow(rows)
+		if err != nil {
+			return nil, err
+		}
+		emails = append(emails, *email)
+	}
+
+	return emails, nil
 }
