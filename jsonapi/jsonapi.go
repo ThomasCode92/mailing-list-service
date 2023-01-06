61260635b522cfddc2b1a03284da7e809de6c704
ThomasCode92 <thomascode92@hotmail.com>
2023-01-06 12:31:25 +0100

    JSON Endpoints

--- jsonapi/jsonapi.go
+++ jsonapi/jsonapi.go
@@ -2,9 +2,12 @@ package jsonapi
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"io"
 	"log"
+	"mailinglist/mdb"
 	"net/http"
 )
 
@@ -56,4 +59,116 @@ func returnErr(w http.ResponseWriter, err error, code int) {
 		return errorMessage, nil
 	}
 	returnJson(w, handleErr)
+}
+
+func CreateEmail(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "POST" {
+			return
+		}
+
+		entry := mdb.EmailEntry{}
+		fromJson(req.Body, &entry)
+
+		if err := mdb.CreateEmail(db, entry.Email); err != nil {
+			returnErr(w, err, 400)
+		}
+
+		returnJson(w, func() (interface{}, error) {
+			log.Printf("JSON CreateEmail: %v\n", entry.Email)
+			return mdb.GetEmail(db, entry.Email)
+		})
+	})
+}
+
+func GetEmail(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "GET" {
+			return
+		}
+
+		entry := mdb.EmailEntry{}
+		fromJson(req.Body, &entry)
+
+		returnJson(w, func() (interface{}, error) {
+			log.Printf("JSON GetEmail: %v\n", entry.Email)
+			return mdb.GetEmail(db, entry.Email)
+		})
+	})
+}
+
+func GetEmailBatch(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "GET" {
+			return
+		}
+
+		queryOptions := mdb.GetEmailBatchQueryParams{}
+		fromJson(req.Body, &queryOptions)
+
+		if queryOptions.Count <= 0 || queryOptions.Page <= 0 {
+			returnErr(w, errors.New("page and count fields are required and must be > 0"), 400)
+			return
+		}
+
+		returnJson(w, func() (interface{}, error) {
+			log.Printf("JSON GetEmailBatch: %v\n", queryOptions)
+			return mdb.GetEmailBatch(db, queryOptions)
+		})
+	})
+}
+
+func UpdateEmail(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "PUT" {
+			return
+		}
+
+		entry := mdb.EmailEntry{}
+		fromJson(req.Body, &entry)
+
+		if err := mdb.UpdateEmail(db, entry); err != nil {
+			returnErr(w, err, 400)
+			return
+		}
+
+		returnJson(w, func() (interface{}, error) {
+			log.Printf("JSON UpdateEmail: %v\n", entry.Email)
+			return mdb.GetEmail(db, entry.Email)
+		})
+	})
+}
+
+func DeleteEmail(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "POST" {
+			return
+		}
+
+		entry := mdb.EmailEntry{}
+		fromJson(req.Body, &entry)
+
+		if err := mdb.DeleteEmail(db, entry.Email); err != nil {
+			returnErr(w, err, 400)
+			return
+		}
+
+		returnJson(w, func() (interface{}, error) {
+			log.Printf("JSON DeleteEmail: %v\n", entry.Email)
+			return mdb.GetEmail(db, entry.Email)
+		})
+	})
+}
+
+func Serve(db *sql.DB, bind string) {
+	http.Handle("/email/create", CreateEmail(db))
+	http.Handle("/email/get", GetEmail(db))
+	http.Handle("/email/get_batch", GetEmailBatch(db))
+	http.Handle("/email/update", UpdateEmail(db))
+	http.Handle("/email/delete", DeleteEmail(db))
+
+	err := http.ListenAndServe(bind, nil)
+	if err != nil {
+		log.Fatalf("JSON server error: %v", err)
+	}
 }
