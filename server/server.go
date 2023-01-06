edd3fbed99db8143de23888b14072de3b08f61e2
ThomasCode92 <thomascode92@hotmail.com>
2023-01-06 12:58:42 +0100

    gRPC Endpoints

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"mailinglist/grpcapi"
 	"mailinglist/jsonapi"
 	"mailinglist/mdb"
 	"sync"
@@ -13,6 +14,7 @@ import (
 var args struct {
 	DbPath   string `arg:"env:MAILINGLIST_DB"`
 	BindJson string `arg:"env:MAILINGLIST_BIND_JSON"`
+	BindGrpc string `arg:"env:MAILINGLIST_BIND_GRPC"`
 }
 
 func main() {
@@ -23,6 +25,9 @@ func main() {
 	}
 	if args.BindJson == "" {
 		args.BindJson = ":8080"
+	}
+	if args.BindGrpc == "" {
+		args.BindGrpc = ":8081"
 	}
 
 	log.Printf("using database '%v'\n", args.DbPath)
@@ -40,6 +45,13 @@ func main() {
 	go func() {
 		log.Printf("starting JSON API server...\n")
 		jsonapi.Serve(db, args.BindJson)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		log.Printf("starting gRPC API server...\n")
+		grpcapi.Serve(db, args.BindGrpc)
 		wg.Done()
 	}()
 
